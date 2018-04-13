dda498922b9c553f5402d1c3d7c45e1ebea67abc
xinsongyan <xinsongyan@gmail.com>
2018-04-13 15:18:38 +0200

    add load model from path example

---
+++ examples/custom_examples/example_load_model_from_path.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Load claw.xml from xmls folder.
+"""
+from mujoco_py import load_model_from_xml, load_model_from_path, MjSim, MjViewer
+import math
+import os
+
+
+
+model = load_model_from_path("../../xmls/claw.xml")
+sim = MjSim(model)
+viewer = MjViewer(sim)
+t = 0
+while True:
+    t += 1
+    sim.step()
+    viewer.render()
+    if t > 100 and os.getenv('TESTING') is not None:
+        break

fdcbd33d96040e882cca62ac3989946f03e7d1e5
xinsongyan <xinsongyan@gmail.com>
2018-04-13 19:15:39 +0200

    add state print

--- examples/custom_examples/tosser.py
+++ examples/custom_examples/tosser.py
@@ -4,8 +4,18 @@ Shows how to toss a capsule to a container.
 """
 from mujoco_py import load_model_from_path, MjSim, MjViewer
 import os
+import numpy as np
 
-model = load_model_from_path("../xmls/tosser.xml")
+np.set_printoptions(precision=5)
+def print_state(state):
+    time, qpos, qvel, act, udd_state = state.time, state.qpos, state.qvel, state.act, state.udd_state
+    print("t: %5.3f" %time)
+    print("qpos: ", qpos)
+    print("qvel: ", qvel)
+
+
+
+model = load_model_from_path("../../xmls/tosser.xml")
 sim = MjSim(model)
 
 viewer = MjViewer(sim)
@@ -16,7 +26,11 @@ sim_state = sim.get_state()
 while True:
     sim.set_state(sim_state)
 
-    for i in range(3000):
+    for i in range(1000):
+        state = sim.get_state()
+        # time, qpos, qvel, act, udd_state = state.time, state.qpos, state.qvel, state.act, state.udd_state
+        # print(time, qpos, qvel)
+        print_state(state)
         if i < 150:
             sim.data.ctrl[0] = -0.0
             sim.data.ctrl[1] = -0.0

dabad05dac8175ee761f508eae7743ae2dce8b28
xinsongyan <xinsongyan@gmail.com>
2018-04-16 11:27:39 +0200

    check qpos and qvel and com_pos

--- examples/custom_examples/humanoid.py
+++ examples/custom_examples/humanoid.py
@@ -5,7 +5,24 @@ Load claw.xml from xmls folder.
 from mujoco_py import load_model_from_xml, load_model_from_path, MjSim, MjViewer
 import math
 import os
+import numpy as np
 
+np.set_printoptions(precision=3)
+
+
+def print_state(state):
+    # qpos(28) = [x, y, z, qw, qx, qy, qz, q(21)]
+    # qvel(27) = [xd, yd, zd, omega_x, omega_y, omega_z, qd(21)]
+    time, qpos, qvel, act, udd_state = state.time, state.qpos, state.qvel, state.act, state.udd_state
+    print("t: %5.3f" % time)
+    print("qpos: ", qpos.shape, qpos)
+    print("qvel: ", qvel.shape, qvel)
+
+
+def mass_center(model, sim):
+    mass = np.expand_dims(model.body_mass, 1)
+    xpos = sim.data.xipos
+    return (np.sum(mass * xpos, 0) / np.sum(mass))
 
 
 model = load_model_from_path("../../xmls/humanoid.xml")
@@ -13,6 +30,9 @@ sim = MjSim(model)
 viewer = MjViewer(sim)
 t = 0
 while True:
+    state = sim.get_state()
+    print_state(state)
+    print("com: ", mass_center(model, sim))
     t += 1
     sim.step()
     viewer.render()
